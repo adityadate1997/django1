73ead1ab26d9272f3e43d308135b49d309035c98
adityadate1997 <adityadate1@gmail.com>
2020-06-03 00:17:17 +0530

    Crud

--- music/models.py
+++ music/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,6 +9,10 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
+    def get_absolute_url(self):
+        return reverse("music:detail", kwargs={"pk": self.pk})
+    
+
     def __str__(self):
         return self.album_title + "-" + self.artist
 
@@ -17,7 +21,6 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=100)
-    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.song_title
--- music/urls.py
+++ music/urls.py
@@ -5,12 +5,18 @@ app_name = 'music'
 
 urlpatterns = [
     #/music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /music/<album-id/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/<album-id/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    # /music/album/add/
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name="album-add"),
+
+    # /music/album/3/
+    url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name="album-update"),
+
+    # /music/album/2/delete/
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name="album-delete"),
 
 ]
